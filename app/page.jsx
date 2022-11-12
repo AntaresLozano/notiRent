ef31e9882afdc7f9ee18b42754f21f2c997f01ad
antares <dizziness1948@gmail.com>
2022-11-12 13:21:47 -0500

    ajuste página inicial

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,41 @@
-export default function HomePage () {
-    return(
-        <h1>Esta es la página inicial</h1>
-    )
+import {getHouse} from './data/api'
+
+function HomePage() {
+  return (
+    <div>
+      {/* <h1>{house.title}</h1>
+      <img src={house.url} /> */}
+    </div>
+  );
+}
+
+
+export async function getServerSideProps() {
+    let house;
+    try {
+      house = await getHouse();
+    } catch (e) {
+      /**
+       * If something goes wrong, we return a 404 page
+       */
+      return {
+        notFound: true,
+      };
+    }
+
+    if (!house) {
+        /**
+         * If we don't get a `house` back, we return a 404 page
+         */
+        return {
+          notFound: true,
+        };
+      }
+    
+      return {
+        props: {
+          house,
+        },
+      };
+    
 }
