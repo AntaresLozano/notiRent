c81e4b50e0b59d1e10e1639230827b41cc37378a
antares <dizziness1948@gmail.com>
2022-11-12 15:55:07 -0500

    ajustre

--- app/page.jsx
+++ app/page.jsx
@@ -1,44 +1,41 @@
 import { getHouse } from "./data/api";
 import './styles/page.css';
+import Swal from 'sweetalert2';
+
+export const disparar = () =>{
+    Swal.fire({
+        title: 'Aporta!',
+        text: '¿Quieres aportar por este apartamento?',
+        icon: 'success',
+        confirmButtonText: 'Cool'
+      })
+  }
 
 export default async function HomePage() {
   const casas1 = await getHouse();
 
   const casas = casas1[1];
+   const Swal = require('sweetalert2')
 
-   console.log(casas)
-  // let image = casas.foto1
-  // let nombre = casas.Tipo_Inmueble + ' ' + casas.Zona ;
-  // let precio = casas.Canon;
-  // let ubi = casas.Ciudad + ' ' + casas.Barrio;
-  // let hab = casas.Alcobas;
-  // let area = casas.AreaLote;
-  // let banos = casas.banios;
-  // let description = casas.descripcionlarga
   return (
     <>
       <div className="body">
         {/* <img className="image_inmueble" src={casas.foto1} style={{maxWidth: '300px', margin: '0 auto'}} /> */}
+            <div>
+
+            </div>
+            <h3 style={{textAlign: 'center'}}>Nuevo apartamento encontrado en tu zona</h3>
+
             <div className="card" style={{width: '300px', margin: '0 auto'}}>
                 <div className="card-body">
                     <div>
                     <img src={casas.foto1} className="card-img-top" alt="..."/>
                     <h5 className="card-title">{casas.Canon}</h5>
                     <p className="card-text">{casas.Ciudad + ' ' + casas.Barrio}</p>
-                    <p className="card-text">Casa con {casas.Alcobas} alcobas, y {casas.banios} baños con un área de {casas.AreaConstruida} mt2</p>
-                    <a href="#" className="btn btn-primary">Ver</a>
+                    <p className="card-text">Apartamento con {casas.Alcobas} alcobas, y {casas.banios} baños con un área de {casas.AreaConstruida} mt2</p>
+                    <a href="#" className="btn btn-primary" onClick={disparar()}>Ver</a>
                 </div>
-        </div>
-          {/* <ul className="contenedor_listado">
-            <li className="item_listado">Precio: {casas.Canon} </li>
-            <li className="item_listado">Ubicacion: {casas.Ciudad + ' ' + casas.Barrio} </li>
-            <li className="item_listado">Habitaciones: {casas.Alcobas} </li>
-          </ul>
-          <ul className="contenedor_listado">
-            <li className="item_listado">Area: {casas.AreaLot} </li>
-            <li className="item_listado">Baños: {casas.banios} </li>
-            <li className="item_listado">Desciption: {casas.descripcionlarga} </li>
-          </ul> */}
+            </div>
         </div>
       </div>
     </>
