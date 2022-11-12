e805bcbf351516a2fc2dd2812fd593db0f9efe97
antares <dizziness1948@gmail.com>
2022-11-12 15:30:37 -0500

    Una tarjeta mostrada

--- app/data/api.js
+++ app/data/api.js
@@ -1,17 +1,20 @@
-  const baseUrl = 'https://www.simi-api.com/ApiSimiweb/response/v2.1.3/filtroInmueble/limite/1/total/12/departamento/0/ciudad/';
 
-  const complementUrl = '/zona/0/barrio/0/tipoInm/0/tipOper/0/areamin/0/areamax/0/valmin/0/valmax/0/campo/0/order/desc/banios/0/alcobas/0/garajes/0/sede/0';
-
-  const codeCities = [['Medellín', '25974'], ['Bello', '26019'], ['Envigado', '25999'], ['Sabaneta', '26020']];
 
 export async function getHouse() {
+    const baseUrl = 'https://www.simi-api.com/ApiSimiweb/response/v2.1.3/filtroInmueble/limite/1/total/12/departamento/0/ciudad/';
+
+    const complementUrl = '/zona/0/barrio/0/tipoInm/0/tipOper/0/areamin/0/areamax/0/valmin/0/valmax/0/campo/0/order/desc/banios/0/alcobas/0/garajes/0/sede/0';
+  
+    const codeCities = [['Medellín', '25974'], ['Bello', '26019'], ['Envigado', '25999'], ['Sabaneta', '26020']];
     // const randomHouse = getRandomInt(1, 12);
-    const house = await fetch(baseUrl + codeCities[1] + complementUrl, {
+    const response = await fetch(baseUrl + '25999' + complementUrl, {
         method: 'GET',
         mode: 'cors',
         headers: {              
           'authorization': 'Basic QXV0aG9yaXphdGlvbjpYNTVTVXV3MEN2VEx0MXRZV1FDU3lieE5LQVVORGFBaExrb3hwRmN0LTY0OQ=='
         }
-      }).then((response) => response.json());  
-    return house;
+      });
+
+    const { Inmuebles } = await response.json();
+    return Inmuebles;
   }
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,46 @@
-import { house } from "./data/api";
+import { getHouse } from "./data/api";
+import './styles/page.css';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const casas1 = await getHouse();
+
+  const casas = casas1[1];
+
+   console.log(casas)
+  // let image = casas.foto1
+  // let nombre = casas.Tipo_Inmueble + ' ' + casas.Zona ;
+  // let precio = casas.Canon;
+  // let ubi = casas.Ciudad + ' ' + casas.Barrio;
+  // let hab = casas.Alcobas;
+  // let area = casas.AreaLote;
+  // let banos = casas.banios;
+  // let description = casas.descripcionlarga
   return (
-    <div>
-      <h1> {house} </h1>
-      <h1></h1>
-    </div>
+    <>
+      <div className="body">
+        {/* <img className="image_inmueble" src={casas.foto1} style={{maxWidth: '300px', margin: '0 auto'}} /> */}
+            <div className="card" style={{width: '300px', margin: '0 auto'}}>
+                <div className="card-body">
+                    <div>
+                    <img src={casas.foto1} className="card-img-top" alt="..."/>
+                    <h5 className="card-title">{casas.Canon}</h5>
+                    <p className="card-text">{casas.Ciudad + ' ' + casas.Barrio}</p>
+                    <p className="card-text">Casa con {casas.Alcobas} alcobas, y {casas.banios} baños con un área de {casas.AreaConstruida} mt2</p>
+                    <a href="#" className="btn btn-primary">Ver</a>
+                </div>
+        </div>
+          {/* <ul className="contenedor_listado">
+            <li className="item_listado">Precio: {casas.Canon} </li>
+            <li className="item_listado">Ubicacion: {casas.Ciudad + ' ' + casas.Barrio} </li>
+            <li className="item_listado">Habitaciones: {casas.Alcobas} </li>
+          </ul>
+          <ul className="contenedor_listado">
+            <li className="item_listado">Area: {casas.AreaLot} </li>
+            <li className="item_listado">Baños: {casas.banios} </li>
+            <li className="item_listado">Desciption: {casas.descripcionlarga} </li>
+          </ul> */}
+        </div>
+      </div>
+    </>
   );
 }
